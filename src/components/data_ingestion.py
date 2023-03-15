d3272e177860b91d14fe8e2f089eabfbf0e2fbb7
Nry <nitheshm.19.becs@acharya.ac.in>
2023-03-15 18:54:45 +0530

    updated code 15-03

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,8 +7,12 @@ from dataclasses import dataclass
 
 from src.exception import CustomException
 from src.logger import logging
+
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @ dataclass
 class DataIngestionConfig:
@@ -51,5 +55,10 @@ if __name__ == '__main__':
     obj = DataIngestion()
     train_data , test_data = obj.initiate_data_ingestion()
 
-    data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    data_transformation = DataTransformation()
+    train_arr,test_arr,preprocessor_obj_file_path = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_tr = ModelTrainer()
+    model_R2_square_value = model_tr.initiate_model_trainer(train_arr,test_arr,preprocessor_obj_file_path)
+
+    print(model_R2_square_value)
